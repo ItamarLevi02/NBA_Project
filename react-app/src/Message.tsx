2683e98e044b0297686eb32b6cc4f0a9202bc639
Itamar Levi <itamarlevi@Itamars-MacBook-Pro-2.local>
2024-03-25 12:31:42 -0700

    front end initial commit

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+
+
+// PascalCasing
+function Message(){
+    //JSX: JavaSCript XML
+    const name = 'Itamar'
+    if (name)
+        return <h1>Hello {name}</h1>;
+    else
+        return <h1>Hello World</h1>
+}
+
+export default Message;
